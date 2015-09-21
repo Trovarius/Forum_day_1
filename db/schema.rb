4cc372e16c7bf38a44b13849d42c285fefda1ed7
Trovarius <tordek.coast@gmail.com>
2015-09-21 15:15:20 +0000

    add column cateory in post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921134802) do
+ActiveRecord::Schema.define(version: 20150921151017) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20150921134802) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
