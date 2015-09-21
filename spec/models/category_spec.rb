f45608f4343bf453573b22ace654f28e73c10935
Trovarius <tordek.coast@gmail.com>
2015-09-21 15:13:07 +0000

    add name validation in category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe "#valid?" do
+   
+   context 'name' do
+     
+     it 'is not null' do
+       category = FactoryGirl.build(:category, :name => nil)
+       expect(category).to_not be_valid
+     end
 
-  it "has to gave a name"
+     it 'is too long (length: 100)' do
+       category =  FactoryGirl.build(:category, :name => "a" * 101)
+
+       expect(category).to_not be_valid
+     end
+
+   end
+
+ end
 end
