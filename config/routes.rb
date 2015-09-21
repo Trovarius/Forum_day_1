61b11b24eb81a2d3e61cf5283da501b5dd395d6f
Trovarius <tordek.coast@gmail.com>
2015-09-21 17:08:24 +0000

    create view to show all categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
+
+  get 'categories/:id/posts' => 'categorie#posts', as: :posts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
